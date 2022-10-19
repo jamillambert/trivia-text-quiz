7b11b447a0227f1198e6b281585dbd28873d57b7
jamillambert <Jamil.Lambert@proton.me>
2022-10-19 08:55:23 +0100

    Version 1.0.0 working snap

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
 from src.html_parser import parse_site
 from src.question_model import Question
 from src.quiz import Quiz
-from src.app import main
--- src/question_model.py
+++ src/question_model.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Question:
     def __init__(self, subject, answer_type, difficulty, text, answer):
         self.subject = subject
@@ -8,35 +9,34 @@ class Question:
         self.text = text
         self.answer = answer
         self.multiple_choice = []
-    
-    
+
     def set_multiple_choice(self, incorrect_answers):
         """Adds the incorrect answers together with the correct answer in a random order into self.multiple_choices[]
         
         All the answers are appended with a sequential number in a random order.  The array is then sorted to have the 
         answers in increasing number"""
         options = []
-        for i in range(len(incorrect_answers)+1):
-            options.append(f"{i+1}. ")
+        for i in range(len(incorrect_answers) + 1):
+            options.append(f"{i + 1}. ")
         random.shuffle(options)
         options[0] += self.answer
         self.answer = options[0]
         for i in range(len(incorrect_answers)):
-            options[i+1] += incorrect_answers[i]
+            options[i + 1] += incorrect_answers[i]
         options.sort()
         self.multiple_choice = options
-        
-        
+
     def check_answer(self, user_answer):
         """Returns True if user_answer is correct, and False if not"""
         return user_answer.lower() == self.answer.lower()
-
 
     def valid_answer(self, user_answer):
         """Returns True if the user_answer is a possible choice for an answer
         
         For true of false questions, any of t, f, true or false are accepted, non case sensitive
         for multiple choice questions the answer must be an integer between 1 and (number of options) inclusive"""
+        if user_answer == 'x':
+            return True
         if self.answer_type == 'boolean':
             if user_answer.lower() in ['t', 'f', 'false', 'true']:
                 return True
@@ -44,18 +44,17 @@ class Question:
                 return False
         elif self.answer_type == 'multiple':
             try:
-                if int(user_answer) >= 1 and int(user_answer) <= len(self.multiple_choice):
+                if 1 <= int(user_answer) <= len(self.multiple_choice):
                     return True
                 else:
                     return False
-            except:
+            except ValueError:
                 return False
         elif user_answer != '':
             return True
         else:
             return False
-            
-    
+
     def print_question(self):
         """Prints to the terminal the question text"""
         if self.answer_type == 'boolean':
